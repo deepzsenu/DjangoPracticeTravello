20a71cd011bd6da5467ecdd83fd2ff00ed964f33
deepzsenu <deepzsenu@gmail.com>
2022-09-16 03:51:16 +0530

    created used database as auth_user

--- practice/urls.py
+++ practice/urls.py
@@ -19,8 +19,11 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 
+import account
+
 urlpatterns = [
     path("",include("travello.urls")),
     path('admin/', admin.site.urls),
+    path('account/', include('account.urls'),)
     ]
 urlpatterns = urlpatterns + static(settings.MEDIA_URL, document_root =settings.MEDIA_ROOT)
