1eaa6dac5fb7f83a9b92c19f4629f5438404c66e
deepzsenu <deepzsenu@gmail.com>
2022-09-16 00:56:11 +0530

    connected database in model and 0001initial.y

--- travello/models.py
+++ travello/models.py
@@ -1,11 +1,13 @@
+from distutils.command.upload import upload
+from sys import maxsize
 from xmlrpc.client import boolean
 from django.db import models
 
 # Create your models here.
-class Destination:
-    id :int
-    name: str
-    img:str
-    description:str
-    price:int
-    offer:boolean
+class Destination(models.Model):
+    name =  models.CharField(max_length=100)
+    img =models.ImageField(upload_to = 'static/imgs/place')
+    
+    price =models.IntegerField(default=700)
+    offer = models.BooleanField(default=False)
+    descr = models.TextField(default='description')
