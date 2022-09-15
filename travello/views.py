bcfdfd41447b861b2cbfa6746e741561a6bacee6
deepzsenu <deepzsenu@gmail.com>
2022-09-15 20:39:25 +0530

    first commit

---
+++ travello/views.py
@@ -0,0 +1,10 @@
+# Create your views here.
+
+from http.client import HTTPResponse
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+def index(request):
+    return render(request, "index.html",{'name':"Deepak Kumar Saxena"})
